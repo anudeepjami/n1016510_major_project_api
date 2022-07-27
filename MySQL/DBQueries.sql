8480b2af8ce461205d1d312c3b8182faae5cf813
N1016510 <anudeep.jami@gmail.com>
2022-07-27 17:15:21 +0100

    package additions

---
+++ MySQL/DBQueries.sql
@@ -0,0 +1,10 @@
+-- create database majorproject;
+-- use majorproject;
+
+CREATE TABLE user_table (
+    userid VARCHAR(255) NOT NULL,
+    username VARCHAR(255),
+    email VARCHAR(255)
+);
+
+select * from user_table;
