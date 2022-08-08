8017241d167eee429a24ba3f4e030f3fa286c333
N1016510 <anudeep.jami@gmail.com>
2022-08-09 00:17:36 +0100

    email service

--- routes.js
+++ routes.js
@@ -13,6 +13,31 @@ module.exports = app => {
     res.status(200).send("Welcome to Version 1 of Anudeep Jami N1016510 Major Project");
 });
 
+  router.get("/mail", (req, res) => {
+    // using Twilio SendGrid's v3 Node.js Library
+    // https://github.com/sendgrid/sendgrid-nodejs
+
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey("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")
+    const msg = {
+      to: 'anudeep.jami@gmail.com', // Change to your recipient
+      from: 'admin@anudeepjami-crowdfunding.me', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.status(200).send("email success");
+      })
+      .catch((error) => {
+        res.status(200).send(error);
+      })
+
+  });
+
+
 //   //// accessing Companies Market Cap Controller
 //   const companiescontroller = require("../controllers/companies.controller.js");
 
