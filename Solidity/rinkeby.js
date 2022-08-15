443f2838b6f4b04d6003533013cdbfd5ce81cf72
N1016510 <anudeep.jami@gmail.com>
2022-08-15 19:10:14 +0100

    removing samples

--- Solidity/rinkeby.js
+++ Solidity/rinkeby.js
@@ -1,7 +1,16 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const truffle = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const contract = require('./compiled_contracts/lottery.json');
+//importing abi and bytecode of Crowdfunding Events Contract
+const CrowdFundingEventsContract = require('../Solidity/compiled_contracts/CrowdfundingEvents.json');
+const crowdfundingEventsContract = {
+    interface: CrowdFundingEventsContract.contracts['CrowdfundingEvents.sol']['CrowdfundingEvents'].abi,
+    bytecode: CrowdFundingEventsContract.contracts['CrowdfundingEvents.sol']['CrowdfundingEvents'].evm.bytecode.object
+};
+const crowdfundingEventContract = {
+    interface: CrowdFundingEventsContract.contracts['CrowdfundingEvents.sol']['CrowdfundingEvent'].abi,
+    bytecode: CrowdFundingEventsContract.contracts['CrowdfundingEvents.sol']['CrowdfundingEvent'].evm.bytecode.object
+};
 
 const wallet = new HDWalletProvider(
 'lizard table distance moon stage champion person air discover cave army concert',
@@ -14,10 +23,13 @@ const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
 
-    contract_instance = await new web3.eth.Contract(contract.contracts['lottery.sol']['Lottery'].abi)
-        .deploy({data: contract.contracts['lottery.sol']['Lottery'].evm.bytecode.object})
-        .send( {from: accounts[0], gas: '1000000'});
-    console.log(contract_instance.options.address);
+    // deploy ethereum contract to rinkeby ethereum network
+    var crowdfundingEventsContract_instance = await new web3.eth
+        .Contract(crowdfundingEventsContract.interface)
+        .deploy({ data: crowdfundingEventsContract.bytecode, arguments: ['AJ Hybrid DAO Crowdfunding'] })
+        .send({ from: accounts[0], gas: '4000000' });
+    console.log(crowdfundingEventsContract_instance.options.address);
+
     wallet.engine.stop();
 };
 deploy();
