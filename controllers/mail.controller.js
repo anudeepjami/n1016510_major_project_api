504bbf5c739a282a506f023c98ea46afad5fa657
N1016510 <anudeep.jami@gmail.com>
2022-08-18 02:51:46 +0100

    adding send email logic

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -1,30 +1,49 @@
-router.get("/mail", (req, res) => {
-    // using Twilio SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-
-    const sgMail = require('@sendgrid/mail')
-    sgMail.setApiKey("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")
-    const msg = {
-      to: 'anudeep.jami@gmail.com', // Change to your recipient
-      from: 'admin@anudeepjami-crowdfunding.me', // Change to your verified sender
-      subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
-    sgMail
-      .send(msg)
-      .then(() => {
-        res.status(200).send("email success");
-      })
-      .catch((error) => {
-        res.status(200).send(error);
-      })
-
-  });
+const User = require('../models/user.model.js');
+const Email = require('@sendgrid/mail');
 
 
-//   //// accessing Companies Market Cap Controller
-//   const companiescontroller = require("../controllers/companies.controller.js");
+//Get user wallet details stored in db
+exports.SendEmail = async (req, res) => {
 
-//   //put for get companies
-//   router.put("/companies", companiescontroller.getCompanies)
+    // get user wallet details from db
+    User.GetWallets(async (err, WalletsInfo) => {
+        if (err) {
+            res.status(200).send({ message: err.message });
+        }
+        else {
+            var toAddresses = [];
+            req.body.fundDetails[4].forEach((item, index) => {
+                WalletsInfo.forEach((item2, index2) => {
+                    if(item[0] == item2.wallet_address)
+                        toAddresses.push(item2.email_id);
+                })
+            });
+            const fromAddress = 'admin@anudeepjami-crowdfunding.me';
+            const emailSubject = 'AJ Hybrid Crowdfunding Funding Platform';
+            const emailBody = "<div><h2><a href='https://anudeepjami-crowdfunding.me/'>AJ Hybrid Crowdfunding Funding Platform</a></h2>" +
+                "<h3>Voting Event Alert ...!</h3>" +
+                "<p>As you are a contributor for campaign '"+ req.body.fundDetails[0]+
+                "' with contract address '"+req.body.fundAddress+"'. " +
+                "You are requested to vote for the following voting event created as part of this campaign</p>" +
+                "<p>Voting Event Title: '"+req.body.votingEventDetails[0]+
+                "' <br/> Voting Event Description: '"+req.body.votingEventDetails[1]+"."+
+                "<br/>For More Details <a href='https://anudeepjami-crowdfunding.me/vote?"+
+                "FundAddress="+req.body.fundAddress+"&"+"VotingIndex="+req.body.votingIndex+
+                "'>click here</a> to visit the voting event</p></div>";
+
+            Email.setApiKey("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");
+            Email.send({
+                to: toAddresses,
+                from: fromAddress,
+                subject: emailSubject,
+                html: emailBody
+            })
+                .then(() => {
+                    res.status(200).send({ message: " Emails sent successfully " });
+                })
+                .catch((e) => {
+                    res.status(200).send({ message: e.message });
+                })
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const { route } = require("express/lib/application");
 const userController = require("./controllers/user.controller.js");
+const mailController = require("./controllers/mail.controller.js");
 
 
 module.exports = app => {
@@ -18,6 +19,37 @@ module.exports = app => {
 
   //update user wallet details or insert new user wallet details stored in db
   router.post("/updatewallet", userController.UpdateWalletDetails);
+
+  //send emails to wallet owners email id's for voting event
+  router.post("/sendemail", mailController.SendEmail);
+
+  router.get("/mail", (req, res) => {
+    // using Twilio SendGrid's v3 Node.js Library
+    // https://github.com/sendgrid/sendgrid-nodejs
+
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey("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")
+    const msg = {
+      to: 'anudeep.jami@gmail.com', // Change to your recipient
+      from: 'admin@anudeepjami-crowdfunding.me', // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      html: "<div><h2><a href='https://anudeepjami-crowdfunding.me/'>AJ Hybrid Crowdfunding Funding Platform</a></h2>"+
+      "<h3>Voting Event Alert ...!</h3>"+
+      "<p>As you are a contributor for campaign 'Art event' with contract address '0x28fF8A3947A66E7ad0D345E5c7563554bcdD4228'. "+
+          "You are requested to vote for the following voting event created as part of this campaign</p>"+
+      "<p>Voting Event Title: 'event1' <br/> Voting Event Description: 'event1'</p>"+
+      "<p>For More Details <a href='https://anudeepjami-crowdfunding.me/vote'>click here</a> to visit the voting event</p></div>",
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.status(200).send("email success");
+      })
+      .catch((error) => {
+        res.status(200).send(error);
+      })
+
+  });
 
 
 }
