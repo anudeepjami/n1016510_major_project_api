0071c0eec897d3f0e21c6783e517c368d5baeca8
N1016510 <anudeep.jami@gmail.com>
2022-08-19 03:35:43 +0100

    adding encryption

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,31 +1,39 @@
 const User = require('../models/user.model.js');
-
+var CryptoJS = require("crypto-js");
+var secret = 'test1234';
 
 //Get user wallet details stored in db
 exports.GetWalletDetails = async (req, res) => {
 
     // get user wallet details from db
-    User.GetWalletDetails(req.body, async (err, wallet_address) => {
+    User.GetWalletDetails(req.body, async (err, wallet_details) => {
         if (err) {
             res.status(200).send({ message: err.message });
         }
         else {
-            res.status(200).send(wallet_address);
+            var temp = wallet_details;
+            if (temp.length == 1){
+                temp[0].email_id = CryptoJS.AES.decrypt(temp[0].email_id, secret).toString(CryptoJS.enc.Utf8);
+            }
+            res.status(200).send(temp);
         }
     });
 };
 
 //Update or insert user wallet details stored in db
 exports.UpdateWalletDetails = async (req, res) => {
+
+    var input = req;
+    input.body.email_id = CryptoJS.AES.encrypt(input.body.email_id, secret).toString();
     // Update or insert user wallet details in db
-    User.GetWalletDetails(req.body, async (err, wallet_address) => {
+    User.GetWalletDetails(input.body, async (err, wallet_details) => {
         if (err) {
             res.status(200).send({ message: err.message });
         }
         else {
-            if (wallet_address.length != 0){
+            if (wallet_details.length != 0){
                 // update wallet details of user in db if exists in db
-                User.UpdateWallet(req.body, async (err, message) => {
+                User.UpdateWallet(input.body, async (err, message) => {
                     if (err) {
                         res.status(200).send({ message: err.message });
                     }
@@ -36,7 +44,7 @@ exports.UpdateWalletDetails = async (req, res) => {
             }
             else {
                 // Insert wallet details of user into db if not exists in db
-                User.InsertWallet(req.body, async (err, message) => {
+                User.InsertWallet(input.body, async (err, message) => {
                     if (err) {
                         res.status(200).send({ message: err.message });
                     }
